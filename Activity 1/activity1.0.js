806ea46c2644ff63f783c55e21f60cd20f1afdb3
sajidkhan56 <sajid.web56@gmail.com>
2022-08-25 14:09:37 +0500

    changes

--- Activity 1/activity1.0.js
+++
@@ -1,3 +0,0 @@
-//Write a function that computes the square of the elements stored in an array 
-//array is: 9   10    20    8    7 
-//Square of the elements of array is: 81    100    400    64    49
---
+++ Activity1/activity1.0.php
@@ -0,0 +1,8 @@
+<?php
+//Write a function that computes the square of the elements stored in an array
+//array is: 9   10    20    8    7 
+//Square of the elements of array is: 81    100    400    64    49 
+
+echo "testing";
+
+?>
